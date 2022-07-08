60a577ccbabdb1559aaa3144e59c353aa0179a7d
Arturo Cervantes <jesus.cervantes207003@potros.itson.edu.mx>
2022-07-08 16:37:55 -0600

    Modificaciones y animacion completa del menu

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,33 +1,41 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 export const Inicio = () => {
   return (
     <div className='home'>
+      <div className='main-section'>
       <div className='myself-photo'>
-      <img src = {"/images/Myself.jpg"}/>
-      <img src = {"/images/20200224_075910.jpg"}/>
-      <img src = {"/images/FB_IMG_1580408710397.jpg"}/>
-      <img src = {"/images/20190321_140851.jpg"}/>
-      <img src = {"/images/b252ab9c-6ad7-4f5b-9918-c1e335d68f79.jpg"}/>
-      <img src = {"/images/68ca7042-8919-462e-9dce-8bf9dd45899a.jpg"}/>
+        <img src={"/images/Myself.jpg"} />
+        <img src={"/images/FB_IMG_1580408710397.jpg"} />
+        <img src={"/images/20190321_140851.jpg"} />
+        <img src={"/images/68ca7042-8919-462e-9dce-8bf9dd45899a.jpg"} />
+      </div>
+      <div className='text-main-section'>
+      <div className="wrapper">
+        <div className="static-txt">Hola! Soy </div>
+        <div className="words">
+          <span>Arturo Cervantes 🧑</span>
+          <span>Diseñador Web 🌐 </span>
+          <span>Programador Multiplataforma 💾</span>
+          <span>Editor Multimedia 📼</span>
+        </div>
       </div>
       <h1>
-        Hola! Soy Arturo Cervantes. Desarrollador Web en el Valle del Yaqui
-         ofresco mis servicios de <strong>programación y desarrollo</strong> en todo
-        tipo de proyectos web.
+        Soy programador y desarrollador web en el Valle del Yaqui.
+        Ofresco mis servicios de <strong>programación y desarrollo</strong> en todo
+        tipo de proyectos web, asi como en trabajos de <strong>producción multimedia</strong>.
       </h1>
-      
       <h1>
-        Tambien tengo especialidad en producción multimedia con 
-         experiencia
-        con trabajos para empresas así como en proyectos personales
+        Tambien soy gerente de <strong>Agua Purificada ABG</strong> donde realizo trabajos
+        comúnes, asi como administración, logistica, entre otros.
       </h1>
+      </div>
+      </div>
       <h2>
 
       </h2>
       <h3></h3>
-      
+
       <h3 className='heading'>
         Trabajo en generar visibilidad y formalidad a empresas o microempresas
         con soluciones de tecnologia y presentaciones empresariales
